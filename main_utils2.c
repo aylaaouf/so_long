aafad1fb74d9c969136397644be407c841dd4b34
aylaaouf <ayoub.laaoufi@hotmail.com>
2025-03-15 07:12:44 +0000

    refactor: fix norminette

--- main_utils2.c
+++ main_utils2.c
@@ -6,7 +6,7 @@
 /*   By: aylaaouf <aylaaouf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 08:40:59 by aylaaouf          #+#    #+#             */
-/*   Updated: 2025/03/14 03:19:37 by aylaaouf         ###   ########.fr       */
+/*   Updated: 2025/03/15 06:49:28 by aylaaouf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,23 @@ int	no_collectible_more(t_game *game)
 				return (0);
 			j++;
 		}
+		i++;
+	}
+	return (1);
+}
+
+int	is_rectangular(t_map *map)
+{
+	int	i;
+
+	if (!map || !map->map || !map->map[0])
+		return (0);
+	map->width = strlen(map->map[0]);
+	i = 1;
+	while (i < map->height)
+	{
+		if ((int)strlen(map->map[i]) != map->width)
+			return (0);
 		i++;
 	}
 	return (1);
