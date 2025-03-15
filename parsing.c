c279d775bc270da9c5bc81d03d93c165ab5bd053
aylaaouf <ayoub.laaoufi@hotmail.com>
2025-03-15 04:05:40 +0000

    Refactor: fix norminette errors

--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: aylaaouf <aylaaouf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 12:58:31 by aylaaouf          #+#    #+#             */
-/*   Updated: 2025/03/12 03:59:37 by aylaaouf         ###   ########.fr       */
+/*   Updated: 2025/03/15 03:23:24 by aylaaouf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ int	is_rectangular(t_map *map)
 
 	if (!map || !map->map || !map->map[0])
 		return (0);
-
 	map->width = strlen(map->map[0]);
 	i = 1;
 	while (i < map->height)
@@ -54,26 +53,29 @@ int	is_wall(t_map *map)
 	return (1);
 }
 
-
-void	flood_fill(t_map *map, int x, int y)
+int	check_path(char *filename)
 {
-	if (x < 0 || x >= map->width || y < 0 || y >= map->height)
-		return ;
-	if (map->map[y][x] != '0')
-		return ;
-	map->map[y][x] = 'F';
-	flood_fill(map, x + 1, y);
-	flood_fill(map, x - 1, y);
-	flood_fill(map, x, y + 1);
-	flood_fill(map, x, y - 1);
+	int	len;
+
+	len = strlen(filename);
+	if (len <= 4 || strncmp(filename + len - 4, ".ber", 4) != 0
+		||filename[len - 5] == '/')
+		return (0);
+	return (1);
 }
 
-void	valid_map(t_game *game, char *filename)
+void	valid_map_helper(t_game *game, char *filename)
 {
-	game->map = read_map(filename);
 	if (!game->map)
 	{
 		printf("Error: Failed to read map\n");
+		free_map(game->map);
+		clean_game(game);
+		exit(1);
+	}
+	if (!check_path(filename))
+	{
+		printf("Error: Invalid path\n");
 		free_map(game->map);
 		clean_game(game);
 		exit(1);
@@ -85,6 +87,12 @@ void	valid_map(t_game *game, char *filename)
 		clean_game(game);
 		exit(1);
 	}
+}
+
+void	valid_map(t_game *game, char *filename)
+{
+	game->map = read_map(filename);
+	valid_map_helper(game, filename);
 	if (!is_exist(game->map))
 	{
 		printf("na9sa chihaja f map\n");
