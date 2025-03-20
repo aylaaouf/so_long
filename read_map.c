6dd9e5e6c6cba85b81f0900c93b3bc6789a93f06
aylaaouf <ayoub.laaoufi@hotmail.com>
2025-03-20 01:43:45 +0000

    Refactor: Optimize map reading algorithm

--- read_map.c
+++ read_map.c
@@ -6,72 +6,84 @@
 /*   By: aylaaouf <aylaaouf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 07:57:06 by aylaaouf          #+#    #+#             */
-/*   Updated: 2025/03/15 07:33:30 by aylaaouf         ###   ########.fr       */
+/*   Updated: 2025/03/20 01:38:55 by aylaaouf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-static t_map	*allocate_map_data(char *filename)
+void	free_map_2(char **map)
 {
-	t_map	*map_data;
+	int	i;
 
-	map_data = malloc(sizeof(t_map));
-	if (!map_data)
-		return (NULL);
-	map_data->height = count_lines(filename);
-	map_data->map = malloc((map_data->height + 1) * sizeof(char *));
-	if (!map_data->map)
-	{
-		free(map_data);
-		return (NULL);
-	}
-	return (map_data);
+	if (!map)
+		return ;
+	i = 0;
+	while (map[i])
+		free(map[i++]);
+	free(map);
 }
 
-static int	fill_map(int fd, t_map *map_data)
+static void	clean_line(char *line)
+{
+	int	len;
+
+	if (!line)
+		return ;
+	len = ft_strlen(line);
+	if (len > 0 && line[len - 1] == '\n')
+		line[len - 1] = '\0';
+}
+
+static char	**fill_map(char ***map, int fd)
 {
 	char	*line;
-	int		i;
-	char	*newline;
+	int		size;
 
-	i = 0;
+	size = 0;
 	line = get_next_line(fd);
 	while (line)
 	{
-		newline = ft_strchr_l(line, '\n');
-		if (newline)
-			*newline = '\0';
-		if (i == 0)
-			map_data->width = ft_strlen(line);
-		map_data->map[i] = ft_strdup(line);
-		free(line);
-		if (!map_data->map[i])
-			return (0);
-		i++;
+		clean_line(line);
+		if (line[0] == '\0')
+		{
+			free(line);
+			break ;
+		}
+		*map = realloc(*map, (size + 1) * sizeof(char *));
+		if (!(*map))
+			return (free(*map), free_map_2(*map), NULL);
+		(*map)[size++] = line;
 		line = get_next_line(fd);
 	}
-	map_data->map[i] = NULL;
-	return (1);
+	*map = realloc(*map, (size + 1) * sizeof(char *));
+	if (!(*map))
+		return (free_map_2(*map), NULL);
+	(*map)[size] = NULL;
+	return (*map);
 }
 
 t_map	*read_map(char *filename)
 {
 	int		fd;
 	t_map	*map_data;
+	char	**map;
 
+	map = NULL;
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 		return (NULL);
-	map_data = allocate_map_data(filename);
+	map_data = malloc(sizeof(t_map));
+	ft_memset(map_data, 0, sizeof(t_map));
 	if (!map_data)
 		return (close(fd), NULL);
-	if (!fill_map(fd, map_data))
+	map_data->map = fill_map(&map, fd);
+	get_next_line(-1);
+	close(fd);
+	if (!map_data->map)
 	{
-		free_map(map_data);
-		close(fd);
+		free(map_data);
 		return (NULL);
 	}
-	close(fd);
 	return (map_data);
 }
