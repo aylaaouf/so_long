a30e2209070170ea90a6deb51ce27e09ce9d7821
aylaaouf <ayoub.laaoufi@hotmail.com>
2025-03-20 01:57:19 +0000

    fix wrong loop in func is_wall and change msjs of exit status

--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: aylaaouf <aylaaouf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 12:58:31 by aylaaouf          #+#    #+#             */
-/*   Updated: 2025/03/18 21:19:45 by aylaaouf         ###   ########.fr       */
+/*   Updated: 2025/03/20 01:53:02 by aylaaouf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,14 @@ int	is_wall(t_map *map)
 		return (0);
 	get_size_width_height(map->map, &map->height, &map->width);
 	i = 0;
-	while (map->map[i])
+	while (i < map->width)
 	{
 		if (map->map[0][i] != '1' || map->map[map->height - 1][i] != '1')
 			return (0);
 		i++;
 	}
 	j = 0;
-	while (map->map[j])
+	while (j < map->height)
 	{
 		if (map->map[j][0] != '1' || map->map[j][map->width - 1] != '1')
 			return (0);
@@ -50,23 +50,25 @@ int	check_path(char *filename)
 
 void	valid_map_helper(t_game *game, char *filename)
 {
+	get_size_width_height(game->map->map, &game->map->height,
+		&game->map->width);
 	if (!game->map)
 	{
-		ft_printf("Error: Failed to read map\n");
+		ft_printf("Error: Failed to read map.\n");
 		free_map(game->map);
 		clean_game(game);
 		exit(0);
 	}
 	if (!check_path(filename))
 	{
-		ft_printf("Error: Invalid path\n");
+		ft_printf("Error: Invalid path.\n");
 		free_map(game->map);
 		clean_game(game);
 		exit(0);
 	}
 	if (!is_rectangular(game->map))
 	{
-		ft_printf("lmap mam9adach\n");
+		ft_printf("Error: Map is not rectangular.\n");
 		free_map(game->map);
 		clean_game(game);
 		exit(0);
@@ -91,26 +93,23 @@ void	valid_map(t_game *game, char *filename)
 {
 	game->map = read_map(filename);
 	valid_map_helper(game, filename);
+	get_size_width_height(game->map->map,
+		&game->map->height, &game->map->width);
 	if (!is_exist(game->map))
 	{
-		ft_printf("na9sa chihaja f map\n");
+		ft_printf("Error: Map is not complete.\n");
 		free_map(game->map);
 		clean_game(game);
 		exit(0);
 	}
 	if (!is_wall(game->map))
 	{
-		ft_printf("lhit makamelch\n");
+		ft_printf("Error: Wall is not complete.\n");
 		free_map(game->map);
 		clean_game(game);
 		exit(0);
 	}
 	find_player_position(game);
 	if (!is_valid_path(game->map, game->player_x, game->player_y))
-	{
-		ft_printf("Error flood fill\n");
-		free_map(game->map);
-		clean_game(game);
-		exit(0);
-	}
+		helper_valid_path(game);
 }
